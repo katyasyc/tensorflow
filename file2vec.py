da19a8e8c7a9a579f279c8561da79682c4ca0b71
katyasyc <katyasyc@gmail.com>
2016-05-25 16:26:49 -0700

    transfer from word-vectors to eliminate history of big file

---
+++ file2vec.py
@@ -0,0 +1,93 @@
+#takes test file, key as file indexing words to bytes in data file,
+#returns byte indices in data file of words in test file
+
+import sys
+from random import *
+
+#turns a string (line of text stripped of punctuation) into an array of strings
+#each string in the array "words" is a word
+def to_list(line):
+   list_of_words = []
+   word = ''
+   for i in range (0,len(line)):
+      if line[i] == ' ':
+         list_of_words.append(word)
+         word = ''
+      else:
+         word += line[i]
+   list_of_words.append(word)
+   return list_of_words
+
+#lowercases strings, removes non-letter characters
+def clean_up(string1):
+   string1 = string1.lower()
+   string1 = string1.replace('\n',' ')
+   string1 = string1.replace('  ',' ')
+   string1 = string1.replace('\\','')
+
+   """
+   fix this at later date, or never
+   letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+   for i in range (0,len(string1)):
+      string1iletter = False
+      for j in range (0,len(letters)):
+         if string1[i] == letters[j]:
+            string1iletter = True
+      if string1iletter == False:
+         string1 = string1.replace(string1[i],'')
+   string1 = string1.replace('  ','')
+   """
+   return string1
+"""
+#turns a list of words into one-hot vectors
+def find_vocab(words):
+   vocab = []
+   for i in range(0,len(words)):
+      word = words[i]
+      if words[i] not in vocab:
+         vocab.append(word)
+   return vocab
+
+def vector(words,vocab,i):
+   vector = [0] * len(words)
+   vector[vocab.index(words[i])] = 1
+   return vector
+"""
+
+def map_file(key_file):
+    keys = {}
+    string1 = key_file.readline()
+    while string1 != '':
+        split = string1.index(' ')
+        keys[string1[0:split]] = string1[split+1:len(string1)]
+        string1 = key_file.readline()
+    return keys
+
+def main():
+#how do I import any file (specify in console rather than program?
+#bug: output is one shorter if key[words[i]] exists
+   test_file = open('test.data', 'r')
+   vector_file = open('vectors.data', 'w')
+   key_file = open('key.txt', 'r')
+   data_file = open('data.txt', 'r')
+   keys = map_file(key_file)
+   string1 = test_file.read()
+   string1 = clean_up(string1)
+   list_of_words = to_list(string1)
+   for i in range (0,len(list_of_words)):
+       if list_of_words[i] in keys:
+            data_file.seek(int(keys[list_of_words[i]]))
+            vector_file.write(data_file.readline())
+       else:
+            vector_file.write("0 " * 299 + '0\n')
+
+   """
+   vocab = find_vocab(words)
+   print vocab
+
+   word_vectors = []
+   for i in range(0,len(vocab)):
+      word_vectors.append(vector(words,vocab,i))
+"""
+if __name__ == "__main__": main()
---
+++ mapword2vec.py
@@ -0,0 +1,27 @@
+#to convert word2vec .txt to a dictionary, and then write that to a file
+
+import sys
+import os
+
+def add(line,key_file,data_file):
+    split = line.index(' ')
+    data_file.write(line[split+1:len(line)])
+    data_file.seek(0, os.SEEK_END)
+    filesize = data_file.tell()
+    key_file.write(line[0:split] + ' ' + str(filesize) + '\n')
+
+def main():
+   key_file = open('key.txt', 'w')
+   data_file = open('data.txt', 'r+')
+   data_file.truncate()
+   input_file = open('output.txt', 'r')
+   input_file.readline()
+   line = input_file.readline()
+   while line != '':
+      add(line,key_file,data_file)
+      line = input_file.readline()
+   key_file.close()
+   data_file.close()
+   input_file.close()
+
+if __name__ == "__main__": main()
